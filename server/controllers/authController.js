efb2f45326c3625340f199eab6540c1255ae00f2
Kirill Andreev <kenokoz@yandex.ru>
2022-01-07 19:54:53 +0300

    add logout method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -39,6 +39,10 @@ class AuthController {
 
   async logout(req, res, next) {
     try {
+      const { refreshToken } = req.cookies;
+      const token = await userService.logout(refreshToken);
+      res.clearCookie('refreshToken');
+      res.json(token);
     } catch (e) {
       next(e);
     }
